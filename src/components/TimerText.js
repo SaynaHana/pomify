82d1a5bc8e4fef50d31f8ce9490413e54ed3e211
Sayna <nwott1@gmail.com>
2024-11-19 20:34:55 -0500

    Changed buttons and font

--- src/components/TimerText.js
+++ src/components/TimerText.js
@@ -2,7 +2,7 @@ import "./../App.css";
 
 function TimerText({minutes, seconds}) {
     return(
-        <div>
+        <div id="timer-text">
             <h1>{minutes + ":" + String(seconds).padStart(2, "0")}</h1>
         </div>
     );
