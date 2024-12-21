6e0c3107f4c41f7feb6c21a24e20aa1d415a0839
Sayna <nwott1@gmail.com>
2024-12-21 15:00:17 -0500

    Google login popup

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,11 +1,27 @@
 import React from "react";
 import DefaultButton from "./DefaultButton";
+import { auth, googleProvider } from "../utils/Firebase";
+import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
 function LoginButton() {
+    function googleSignIn() {
+        signInWithPopup(auth, googleProvider)
+            .then((result) => {
+                // given a google access token on success
+                const credential = GoogleAuthProvider.credentialFromResult(result);
+                const token = credential.accessToken;
+                const user = result.user;
+            }).catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+
+                console.log("Error " + errorCode + ": " + errorMessage);
+            });
+    }
 
     return(
         <div className="login-button">
-            <DefaultButton text="Login"/>
+            <DefaultButton text="Login" onClick={googleSignIn}/>
         </div>
     );
 }
